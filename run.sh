c5bceca04777403ad084c18c424542584bd296ad
Alicia Amarilla <smushyaa@gmail.com>
2026-05-26 18:11:49 -0400

    feat: added warnings to run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "./example" ] || [ example.c -nt ./example ] || [ flagship.h -nt ./example ]; then
     set -x
-    gcc example.c -o example -ggdb -O0
+    gcc example.c -o example -ggdb -O0 -Wall -Wextra
     { set +x; } 2>/dev/null
 fi
 
